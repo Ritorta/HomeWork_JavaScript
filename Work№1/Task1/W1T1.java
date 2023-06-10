fd4a95fe02bade7c50f8754bcf00031e227d55b1
Ritorta <meddesu@yandex.ru>
2023-06-10 16:11:19 +0300

    Almost Done

--- Work№1/Task1/W1T1.java
+++ Work№1/Task1/W1T1.java
@@ -12,5 +12,98 @@
 
 
 // ------------
-//  Вариант №1
+//  Вариант №1 - Пункты реализовать в методе main
 // ------------
+
+// import java.util.ArrayList;
+// import java.util.Random;
+
+// public class W1T1
+// {
+//     public static void main(String[] args) 
+//     {
+//         int rand = new Random().nextInt(2000 + 1);
+//         int i = rand;
+//         System.out.println("Random number: " + i);
+
+//         int n = Integer.highestOneBit(i);
+//         System.out.println("Hing bit: " + n);
+        
+//         int n_max = 0;
+//         ArrayList<Integer> array1 = new ArrayList<Integer>();
+//         for (int j = n; j < Short.MAX_VALUE; j++) {
+//             if (j % n == 0)
+//             n_max ++;
+//             array1.add(n_max);
+//         }
+//         System.out.println("Multiples of N: " + array1);
+        
+//         int n_min = 0;
+//         ArrayList<Integer> array2 = new ArrayList<Integer>();
+//         for (int j = Short.MIN_VALUE; j < i; j++) {
+//             if (j % n != 0)
+//             n_min ++;
+//             array2.add(n_min);
+//         }
+//         System.out.println("Multiples of N: " + array2);
+//     }
+
+// }
+
+// ------------
+//  Вариант №2 - Пункты реализовать в разных методах
+// ------------
+
+import java.util.ArrayList;
+import java.util.Random;
+
+public class W1T1
+{
+    public static void main(String[] args) 
+    {
+
+    }
+    public void name() 
+    {
+         int rand = new Random().nextInt(2000 + 1);
+        int i = rand;
+        System.out.println("Random number: " + i);
+
+       
+    }
+    }
+
+    public void name() 
+    {
+         int n = Integer.highestOneBit(i);
+        System.out.println("Hing bit: " + n);
+        
+        
+
+    public void name() 
+    {
+        int n_max = 0;
+        ArrayList<Integer> array1 = new ArrayList<Integer>();
+        for (int j = n; j < Short.MAX_VALUE; j++) {
+            if (j % n == 0)
+            n_max ++;
+            array1.add(n_max);
+        }
+        System.out.println("Multiples of N: " + array1);
+        
+        
+    }
+    }
+
+    public void name() 
+    {
+        int n_min = 0;
+        ArrayList<Integer> array2 = new ArrayList<Integer>();
+        for (int j = Short.MIN_VALUE; j < i; j++) {
+            if (j % n != 0)
+            n_min ++;
+            array2.add(n_min);
+        }
+        System.out.println("Multiples of N: " + array2);
+    }
+}
