28be7690b40c98bedbf06ad262dda842c3401c6c
Ritorta <Meddesu@yandex.ru>
2023-06-14 16:41:24 +0500

    Start

--- Work№2/Task1/W2T1.java
+++ Work№2/Task1/W2T1.java
@@ -18,8 +18,44 @@
 // *Сравнить время выполнения замены символа "а" на "А" любой строки содержащей >1000 символов средствами String и StringBuilder.
 // *Дана строка: ".3 + 1.56 = " подсчитать результат и добавить к строке.
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+// import java.util.Arrays;
 
 public class W2T1 
 {
+
+
+    // public static void main(String[] args) {
+    //     Map<String, String> map = new LinkedHashMap<>();
+    //     map.put("name","Ivanov");
+    //     map.put("country","Ukraine");
+    //     map.put("city","Kiev");
+    //     map.put("age",null);
+    //     map.put("name1","Ivanov");
+    //     map.put("name2","Ivanov");
+
+    //     System.out.println(getQuery(map));
+    // }
+
+    // public static String getQuery(Map<String, String> params) {
+    //     StringBuilder result = new StringBuilder();
+    //     if (params == null || params.isEmpty())
+    //         return result.toString();
+
+    //     for (Map.Entry<String, String> pair : params.entrySet()) {
+    //         if (pair.getKey() == null || pair.getValue() == null)
+    //             continue;
+
+    //         result.append(pair.getKey()).append(" = '").append(pair.getValue()).append("' and ");
+    //     }
+
+    //     if (result.length() > 5)
+    //         result.delete(result.length() - 5, result.length());
+
+    //     return result.toString();
+    // }
     
-}
+
+    }
