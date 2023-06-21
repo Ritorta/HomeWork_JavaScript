a1e154f1d3c4989cb138809a4947491510d497ee
Ritorta <Meddesu@yandex.ru>
2023-06-21 17:29:46 +0500

    Heh

--- Work№5/Task1/W5T1.java
+++ Work№5/Task1/W5T1.java
@@ -8,10 +8,7 @@
 //  Вариант №1 - Основное задание
 // ------------ 
 
-import java.util.HashMap;
-import java.util.Map;
-import java.util.TreeMap;
-
+import java.util.*;
 
 
 public class W5T1 
@@ -20,22 +17,78 @@ public class W5T1
     {
         Map<Integer, String> Phonebook = new HashMap<>();
 
-        Phonebook.put(81593, "Vasa");
-        Phonebook.put(39312, "Ira");
-        Phonebook.put(279192-03-83, "Valera");
-        Phonebook.put(412, "Anton");
-        Phonebook.put(90077699-8-39, "Lena");
-        Phonebook.put(829037186-27-5, "Vasa");
-        Phonebook.put(911, "DIMON!");
-        Phonebook.put(508995-51-21, "Alecsey");
-        Phonebook.put(128511, "Andrey");
-        Phonebook.put(109176626-85-86, "Lera");
-        Phonebook.put(313306089, "Sveta");
-        Phonebook.put(1264894140, "Lena");
-        Phonebook.put(1123416240, "Lena");
-        Map<Integer, String> sorted = new TreeMap<>(Phonebook).descendingMap(); 
+        // Phonebook.put(55, "Vasa");
+        // Phonebook.put(123, "Ira");
+        // Phonebook.put(2, "Valera");
+        // Phonebook.put(412, "Anton");
+        // Phonebook.put(6989, "Lena");
+        // Phonebook.put(829037186-27-5, "Vasa");
+        // Phonebook.put(911, "DIMON!");
+        // Phonebook.put(311, "Alecsey");
+        // Phonebook.put(128511, "Andrey");
+        // Phonebook.put(8213, "Lera");
+        Phonebook.put(7886, "Sveta");
+        Phonebook.put(1, "Lena");
+        Phonebook.put(456, "Lena");
+
+           
+        // for (int i : Phonebook.keySet())
+        // {
+        //         if(Phonebook.containsValue(value))
+        //             {
+        //                 System.out.println(Phonebook.get(i) + " - " + i);
+
+        //             }
+            
+        // }
+       
+      HashMap<Integer, String> duplicatMap = new HashMap<>();
+
+        Set<Map.Entry<Integer, String>> entrySet = Phonebook.entrySet();
+        Iterator<Map.Entry<Integer, String>> iterator = entrySet.iterator();
+        while(iterator.hasNext()) {
+            Map.Entry<Integer, String> entry = iterator.next();
+            String value = entry.getValue();
+            Integer key = entry.getKey();
+            
+            if(duplicatMap.containsValue(value)) 
+            {
+                duplicatMap.put(key, value);
+            } 
+            else 
+            {
+                duplicatMap.put(key, duplicatMap.get(value)+", " + value);
+            }
+          
+        }
+       
+        Map<Integer, String> sorted = new TreeMap<>(duplicatMap).descendingMap(); 
         sorted.entrySet().forEach(System.out::println); 
 
-    }
+        }
+        
+        }
+        
+
+
+
+
+
+
+
+
+
+
+
+        
+
+// System.out.println(Phonebook);
+// System.out.println(Phonebook);
+
+		
+				
     
-}
+
+    
+    
+
