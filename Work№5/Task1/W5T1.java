ff00b70f6652267f958c61580a186a424fc81865
Ritorta <119888217+Ritorta@users.noreply.github.com>
2023-06-29 12:30:21 +0300

    Update W5T1.java
    New version home work 5, its work.

--- Work№5/Task1/W5T1.java
+++ Work№5/Task1/W5T1.java
@@ -8,14 +8,68 @@
 //  Вариант №1 - Основное задание
 // ------------ 
 
-import java.util.*;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
 
-
-public class W5T1 
+public class W5T1
 {
-    public static void main(String[] args) 
+
+    public static void addnamebook(String key, int value, Map<String, ArrayList<Integer>> map)
     {
-        Map<Integer, String> Phonebook = new HashMap<>();
+        if (map.containsKey(key)) {
+            map.get(key).add(value);
+        } else {
+            ArrayList<Integer> list = new ArrayList<>();
+            list.add(value);
+            map.put(key, list);
+        }
+
+    }
+
+    public static void printphone(Map<String, ArrayList<Integer>> map)
+    {
+        for (var item : map.entrySet()) {
+            String phones = "";
+            for(int el: item.getValue()){
+                phones = phones + el + ", ";
+            }
+            System.out.printf("%s: %s \n", item.getKey(), phones);
+        }
+    }
+
+    public static void main(String[] args)
+    {
+        Map<String, ArrayList<Integer>> phonebook = new HashMap<>();
+        addnamebook("Ira", 5416, phonebook);
+        addnamebook("Kola", 1235164, phonebook);
+        addnamebook("Sidorov", 516816585, phonebook);
+        addnamebook("Ira", 89568477, phonebook);
+        addnamebook("Dima", 146233, phonebook);
+        addnamebook("Kola", 764287897, phonebook);
+        addnamebook("Sidorov", 8456477, phonebook);
+        addnamebook("Veronika", 123566233, phonebook);
+        addnamebook("Ugor", 78789947, phonebook);
+        addnamebook("Ivanov", 8956477, phonebook);
+        addnamebook("Pentorv", 12356233, phonebook);
+        addnamebook("Ivanov", 787897, phonebook);
+        printphone(phonebook);
+    }
+
+}
+
+// ------------
+//  Вариант №2 - На потом
+// ------------ 
+
+// import java.util.*;
+
+
+// public class W5T1 
+// {
+//    public static void main(String[] args) 
+//    {
+//        Map<Integer, String> Phonebook = new HashMap<>();
 
         // Phonebook.put(55, "Vasa");
         // Phonebook.put(123, "Ira");
@@ -27,9 +81,9 @@ public class W5T1
         // Phonebook.put(311, "Alecsey");
         // Phonebook.put(128511, "Andrey");
         // Phonebook.put(8213, "Lera");
-        Phonebook.put(7886, "Sveta");
-        Phonebook.put(1, "Lena");
-        Phonebook.put(456, "Lena");
+//        Phonebook.put(7886, "Sveta");
+//        Phonebook.put(1, "Lena");
+//        Phonebook.put(456, "Lena");
 
            
         // for (int i : Phonebook.keySet())
@@ -42,32 +96,32 @@ public class W5T1
             
         // }
        
-      HashMap<Integer, String> duplicatMap = new HashMap<>();
+//      HashMap<Integer, String> duplicatMap = new HashMap<>();
 
-        Set<Map.Entry<Integer, String>> entrySet = Phonebook.entrySet();
-        Iterator<Map.Entry<Integer, String>> iterator = entrySet.iterator();
-        while(iterator.hasNext()) {
-            Map.Entry<Integer, String> entry = iterator.next();
-            String value = entry.getValue();
-            Integer key = entry.getKey();
+//        Set<Map.Entry<Integer, String>> entrySet = Phonebook.entrySet();
+//        Iterator<Map.Entry<Integer, String>> iterator = entrySet.iterator();
+//        while(iterator.hasNext()) {
+//            Map.Entry<Integer, String> entry = iterator.next();
+//            String value = entry.getValue();
+//            Integer key = entry.getKey();
             
-            if(duplicatMap.containsValue(value)) 
-            {
-                duplicatMap.put(key, value);
-            } 
-            else 
-            {
-                duplicatMap.put(key, duplicatMap.get(value)+", " + value);
-            }
+//            if(duplicatMap.containsValue(value)) 
+//            {
+//                duplicatMap.put(key, value);
+//            } 
+//            else 
+//            {
+//                duplicatMap.put(key, duplicatMap.get(value)+", " + value);
+//            }
           
-        }
+//        }
        
-        Map<Integer, String> sorted = new TreeMap<>(duplicatMap).descendingMap(); 
-        sorted.entrySet().forEach(System.out::println); 
+//        Map<Integer, String> sorted = new TreeMap<>(duplicatMap).descendingMap(); 
+//        sorted.entrySet().forEach(System.out::println); 
 
-        }
+//        }
         
-        }
+//        }
         
 
 
